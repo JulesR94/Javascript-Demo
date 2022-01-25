bcebab415bfa4674613b8e5814d708b999d4f7da
JulesR94 <96482907+JulesR94@users.noreply.github.com>
2022-01-25 11:09:22 +0100

    Update main.js

--- seance2_javascript/essai3/js/main.js
+++ seance2_javascript/essai3/js/main.js
@@ -1,7 +1,15 @@
 let link = document.getElementById('myLink');
 alert(link);
+alert(typeof(link)); 
 
+// let href = link.getAttribute('href');
+// alert(href); 
+
+// link.setAttribute('href', 'https://www.afpa.fr');
+// alert(link)
 let href = link.getAttribute('href');
 alert(href);
 
-link.setAttribute('href', 'https://www.afpa.fr');
+link.setAttribute('href',"https://www.google.fr");
+link = document.getElementById('myLink');
+alert(link);
